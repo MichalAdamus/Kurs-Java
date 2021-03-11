3fa9bcc6cfe5e8534837ce82ab8a782a3208500a
Szymon Leyk <biuro@szymonleyk.pl>
2021-03-11 10:11:08 +0100

    #3 zadanie 2, przykłady z boolean i double

--- Variable.java
+++ Variable.java
@@ -1,5 +1,7 @@
 class Variable{
 		public static void main(String args[]){
+			
+			/*************************** zadanie 1 *************************/
 			int birthYear = 2001;
 			char gender = 'K';
 			
@@ -15,5 +17,28 @@ class Variable{
 			System.out.println("Rok urodzenia: "+birthYear);
 			System.out.println("Płeć: "+gender);
 			
+			
+			
+			/*************************** zadanie 2 *************************/
+			System.out.println();
+			
+			boolean isAdult = true;
+			double average = 3.5;
+			
+			System.out.println("Czy pełnoletnia? "+isAdult);
+			System.out.println("Średnia ocen: "+average);
+			
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 }
