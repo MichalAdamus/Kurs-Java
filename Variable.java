e3eccba2c87c254a51651fe27b48011ab18f23fb
Szymon Leyk <biuro@szymonleyk.pl>
2021-03-11 10:07:34 +0100

    #3 przechodanie informacji o płci i wieku

---
+++ Variable.java
@@ -0,0 +1,19 @@
+class Variable{
+		public static void main(String args[]){
+			int birthYear = 2001;
+			char gender = 'K';
+			
+			System.out.println(birthYear);
+			System.out.println(gender);
+			
+			/*
+			"Witaj "+"Marian"    		=> "Witaj Marian"			
+			"Wiek: "+51          		=> "Wiek : 51"
+			"Rok urodzenia: "+bithYear	=> "Rok urodzenia : 2001"
+			*/
+			
+			System.out.println("Rok urodzenia: "+birthYear);
+			System.out.println("Płeć: "+gender);
+			
+		}
+}
