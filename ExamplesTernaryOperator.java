90044bec115c2248f80a18f18096ecb0090f7811
Szymon Leyk <biuro@szymonleyk.pl>
2021-03-18 09:25:52 +0100

    #6 dodatkowe przykłady z operatora trójargumentowego

--- ExamplesTernaryOperator.java
+++ ExamplesTernaryOperator.java
@@ -17,5 +17,28 @@ class ExamplesTernaryOperator{
 		
 		System.out.println(result);
 		
+		// zad2
+		double salary = 1569.33;
+		double newSalary = salary > 3000 ? salary+400 : salary+800;
+		
+		System.out.println("Salary: "+salary);
+		System.out.println("New salary: "+newSalary);
+		
+		/*
+		-10
+		10
+		
+		10
+		*/
+		
+		// zad3.
+		// samochód 1 - 200000.00
+		// samochód 2- 55000.121
+		double value = 89000.12;
+		int option = value < 200000 ? 2 : 1;
+		
+		System.out.println(option);
+				
+		
 	}
 }
