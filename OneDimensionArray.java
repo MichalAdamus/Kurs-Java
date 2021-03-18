a61f5d58fb454ba2f9cc947e59f8351f6fa2f0fd
Szymon Leyk <biuro@szymonleyk.pl>
2021-03-18 10:22:01 +0100

    #8 zamiana elementów tablicy

--- OneDimensionArray.java
+++ OneDimensionArray.java
@@ -52,5 +52,44 @@ class OneDimensionArray{
 		System.out.println("\tAge: "+age);
 		
 		System.out.println("----------------------------");
+		
+		// surname = M i c h n i e w i c z 
+		// 3 index z 10
+		// surname[3] = surname[10];
+		// surname = M i c z n i e w i c z 
+		// surname[10] = surname[3];
+		// surname = M i c z n i e w i c z 
+		
+		/* zamiana x i y
+		z = x;
+		x = y;
+		y = z;
+		*/
+		
+		// surname = M i c h n i e w i c z 
+		// 3 index z 10
+		char tmp = surname[3];
+		// surname = M i c h n i e w i c z |  tmp = h
+		surname[3] = surname[10];
+		// surname = M i c z n i e w i c z |  tmp = h
+		surname[10] = tmp;
+		// surname = M i c z n i e w i c h |  tmp = h
+		
+		
+		
+		// 1, 5, 2, 4  =>   1, 4, 2, 5  (zamiana 5 i 4)
+		// 1, 4, 2, 5  =>  1, 2, 4, 5  (zamiana 4 i 2)
+
+		System.out.print("\tSurname: "+surname[0]+""+surname[1]+""+surname[2]);
+		System.out.print(surname[3]);
+		System.out.print(surname[4]);
+		System.out.print(surname[5]);
+		System.out.print(surname[6]);
+		System.out.print(surname[7]);
+		System.out.print(surname[8]);
+		System.out.print(surname[9]);
+		System.out.print(surname[10]);
+		System.out.print("["+surname.length+"]");
+		System.out.println();
 	}
 }
